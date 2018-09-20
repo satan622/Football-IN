93485f2d901d91bf43affc8897f4c63d5a5b9fe6
struct622 <satan62236@gmail.com>
2018-09-20 10:54:24 +0900

    change to JQuery for page link

---
+++ Football_In1.32/src/main/java/com/spring/domain/Criteria.java
@@ -0,0 +1,45 @@
+package com.spring.domain;
+
+public class Criteria {
+	private int page;
+	private int perPageNum;
+
+	public Criteria() {
+		this.page = 1;
+		this.perPageNum = 10;
+	}
+
+	public int getPage() {
+		return page;
+	}
+
+	public void setPage(int page) {
+		if (page <= 0) {
+			this.page = 1;
+			return;
+		}
+		this.page = page;
+	}
+
+	public int getPerPageNum() {
+		return this.perPageNum;
+	}
+
+	public void setPerPageNum(int perPageNum) {
+		if (perPageNum <= 0 || perPageNum > 100) {
+			this.perPageNum = 10;
+		}
+		this.perPageNum = perPageNum;
+	}
+
+	// MyBatis Mapper를 위한 메소드
+	public int getPageStart() {
+		return (this.page - 1) * perPageNum;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Criteria [page=" + page + ", perPageNum=" + perPageNum + "]";
+	}
+}
