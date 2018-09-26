0d25c2d2f58f778f90704c1ac773665d8ae529a7
struct622 <satan62236@gmail.com>
2018-09-26 22:50:45 +0900

    replace login button in navbar

---
+++ Football_In2.0.2/src/main/java/com/spring/persistence/UserDAOImpl.java
@@ -0,0 +1,23 @@
+package com.spring.persistence;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.spring.domain.UserVO;
+import com.spring.dto.LoginDTO;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+	@Inject
+	private SqlSession session;
+	
+	private static String namespace = "com.spring.persistence.UserDAO";
+	
+	@Override
+	public UserVO login(LoginDTO dto) throws Exception {
+		return session.selectOne(namespace + ".login", dto);
+	}
+
+}
