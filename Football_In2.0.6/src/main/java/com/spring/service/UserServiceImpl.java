a5c646bc5cd466b3d411c693c4d0590c01cf8b90
struct622 <satan62236@gmail.com>
2018-09-27 09:57:28 +0900

    Authorization for reply

---
+++ Football_In2.0.6/src/main/java/com/spring/service/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.spring.service;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.spring.domain.UserVO;
+import com.spring.dto.LoginDTO;
+import com.spring.persistence.UserDAO;
+
+@Service
+public class UserServiceImpl implements UserService {
+	@Inject
+	private UserDAO dao;
+	
+	@Override
+	public UserVO login(LoginDTO dto) throws Exception {
+		return dao.login(dto);
+	}
+
+}
+ 
