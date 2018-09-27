84c85b16710a2e2b4d769051de41959c75d0524f
struct622 <satan62236@gmail.com>
2018-09-27 14:45:45 +0900

    logout

---
+++ Football_In2.1.0/src/main/java/com/spring/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.spring.controller;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.spring.domain.UserVO;
+import com.spring.dto.LoginDTO;
+import com.spring.service.UserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	@Inject
+	private UserService service;
+	
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public void loginGET(@ModelAttribute("dto") LoginDTO dto) {
+		
+	}
+	
+	@RequestMapping(value="/loginPost", method = RequestMethod.POST)
+	public void loginPOST(LoginDTO dto, HttpSession session, Model model) throws Exception {
+		UserVO vo = service.login(dto);
+		
+		if(vo == null) {
+			return;
+		}
+		
+		model.addAttribute("userVO", vo);
+	}
+	
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logout(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws Exception{
+		Object obj = session.getAttribute("login");
+		
+		if(obj != null) {
+			UserVO vo = (UserVO) obj;
+			
+			session.removeAttribute("login");
+			session.invalidate();
+		}
+		
+		return "user/logout";
+	}
+}
